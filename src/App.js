1f01cfa271a4bf7049befe70fc18eb0f596efd8b
Chasecooler <chasemarq@yahoo.com>
2019-07-12 20:06:01 -0400

    render check

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import Shape from "./Shape";
 
 class Selector extends Component {
--- src/Shape.js
+++ src/Shape.js
@@ -1,18 +1,35 @@
-import React, { Component } from 'react';
+import React from "react";
 
-// shape "square", select function()
-// shape made const and given properties via .dot association
+const Shape = props => {
+  console.log(props);
+  const shape = props.shape;
+  const selectShape = props.selectShape; // this is a function
+  console.log(shape, selectShape);
 
-const Shape = (props) => {
-    const shape = props.shape;
-    const selectShape = props.selectShape;
-    // console.log("Line 4 in Shape.js, testing props", props.Shape)
-
-    return (
-        // <div className="square" />
-        <div> className ={Shape} onClick={() => selectShape(shape)} </div>
-    )
-
-    }
+  return (
+    <div className={shape} onClick={() => selectShape(shape)}>
+      hello
+    </div>
+  );
+};
 
 export default Shape;
+
+// import React, { Component } from 'react';
+
+// // shape "square", select function()
+// // shape made const and given properties via .dot association
+
+// const Shape = (props) => {
+//     const shape = props.shape;
+//     const selectShape = props.selectShape;
+//     // console.log("Line 4 in Shape.js, testing props", props.Shape)
+
+//     return (
+//         // <div className="square" />
+//         <div> className ={Shape} onClick={() => selectShape(shape)} </div>
+//     )
+
+//     }
+
+// export default Shape;
